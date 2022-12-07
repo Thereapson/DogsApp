2bd0ad19831d6592773005bcf23ac7d41eca6e81
sebastian <thereapson@gmail.com>
2022-12-07 11:26:53 -0500

    Push5

--- api/src/db.js
+++ api/src/db.js
@@ -12,7 +12,7 @@ let sequelize =
       database: DB_NAME,
       dialect: "postgres",
       host: DB_HOST,
-      port: 0000,
+      port: 5432,
       username: DB_USER,
       password: DB_PASSWORD,
       uri: DATABASE_URL,
