0a0a1748e78f09772e4c8d998ac44f292f1c630f
sebastian <thereapson@gmail.com>
2022-12-21 11:55:54 -0500

    deploy3

--- client/src/index.js
+++ client/src/index.js
@@ -12,8 +12,8 @@ import axios from 'axios';
 import dotenv from "dotenv";
 
 dotenv.config();
-axios.defaults.baseURL = `http://dogsapp-production.up.railway.app`;
-// axios.defaults.baseURL = `http://localhost:3001`;
+axios.defaults.baseURL = process.env.REACT_APP_API || `http://localhost:3001`;
+
 
 ReactDOM.render(
   <BrowserRouter>
