aba157a71108881603ad36f91c03ab0c9402c451
sebastian <thereapson@gmail.com>
2022-12-21 11:03:04 -0500

    railway2

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,8 @@ import axios from 'axios';
 import dotenv from "dotenv";
 
 dotenv.config();
-axios.defaults.baseURL = `https://dogsapp-production.up.railway.app/` || "http://localhost:3001";
+axios.defaults.baseURL = `http://dogsapp-production.up.railway.app`;
+// axios.defaults.baseURL = `http://localhost:3001`;
 
 ReactDOM.render(
   <BrowserRouter>
